daa6ea7a8f4a487ab638872dfb5f9acc618f8fb3
ciber2000 <anime654321a@gmail.com>
2026-05-11 07:26:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,4 +22,19 @@ connectBtn.innerHTML = "CONNECT";
 
 }
 
-};
+};function showInfo(){
+
+const info =
+document.getElementById("vpnInfo");
+
+if(info.style.display === "block"){
+
+info.style.display = "none";
+
+}else{
+
+info.style.display = "block";
+
+}
+
+}
